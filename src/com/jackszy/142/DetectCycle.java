b200c2a4e71790440a2bb74a15f042facf420fd3
jackszy <jackszy@126.com>
2020-04-06 19:24:13 +0800

    环形链表2

---
+++ src/com/jackszy/142/DetectCycle.java
@@ -0,0 +1,24 @@
+import com.jackszy.ListNode;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class DetectCycle {
+    public static void main(String[] args) {
+
+    }
+
+    //set 判重，时间复杂度O(n),空间复杂度O(n)
+    public ListNode detectCycle(ListNode head) {
+        Set<ListNode> set = new HashSet<>();
+        while (head !=null) {
+            if (set.contains(head))
+                return head;
+
+            set.add(head);
+            head = head.next;
+        }
+
+        return null;
+    }
+}
---
+++ src/com/jackszy/142/DetectCycle2.java
@@ -0,0 +1,29 @@
+
+import com.jackszy.ListNode;
+public class DetectCycle2 {
+
+    //Floyd 算法 ，快慢指针算法。
+    public ListNode detectCycle(ListNode head) {
+        ListNode ptr1 = head;
+        ListNode ptr2 = null;
+        ListNode fast = head;
+        ListNode slow = head;
+        while (slow !=null && fast !=null && fast.next !=null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if (slow == fast) {
+                ptr2 = slow;
+                break;
+            }
+        }
+        if (ptr2  == null)
+            return null;
+
+        while (ptr1 !=ptr2) {
+            ptr1 = ptr1.next;
+            ptr2 = ptr2.next;
+        }
+
+        return ptr1;
+    }
+}
